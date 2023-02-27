9aaf90eaaf28fa3972fd36fa04653a7ec47ec0f1
Josue Palomo <josuepalomocs@gmail.com>
2023-02-27 09:32:12 -0600

    add google maps link to barber shop

--- src/components/BarberLocation.tsx
+++ src/components/BarberLocation.tsx
@@ -1,5 +1,7 @@
 import { useGoogleMap } from "@/hooks/useGoogleMap";
 import { GoogleMap, Marker } from "@react-google-maps/api";
+import Link from "next/link";
+import { ArrowTopRightOnSquareIcon } from "@heroicons/react/20/solid";
 
 export default function BarberLocation() {
   const { googleMapIsLoaded, googleMapCenter, googleMapZoom, googleMapOnLoad } =
@@ -24,11 +26,18 @@ export default function BarberLocation() {
     <div className="w-full p-4 bg-white rounded-sm border">
       <div className="text-xs text-neutral-500 mb-4">LOCATION</div>
       <p className="text-sm text-neutral-700 mb-4">
-        10455 N Central Expy, Suite #21, Dallas, 75231
+        10455 N Central Expy # 124, Dallas, TX 75231
       </p>
       <div className="border-2 border-neutral-200 mb-4 rounded-sm">
         {renderGoogleMap()}
       </div>
+      <Link
+        className="text-xs flex items-center gap-2 text-cyan-700"
+        href="https://goo.gl/maps/aeKb9Y83Kmzx4RFB9"
+      >
+        Open in Google Maps
+        <ArrowTopRightOnSquareIcon className="w-[16px] h-[16px]" />
+      </Link>
     </div>
   );
 }
